8d497c991320539d8b262c313e2ea70de0664aae
edmondliang <edmond.liang.sf@gmail.com>
2015-06-16 23:23:39 +0000

    feat: modified and added template & form

--- app/catalog/controller.py
+++ app/catalog/controller.py
@@ -1,6 +1,6 @@
 # Import flask dependencies
 from flask import Blueprint, request, render_template, \
-                  flash, g, session, redirect, url_for,abort
+    flash, g, session, redirect, url_for, abort
 
 # Import the database object from the main app module
 from app import db
@@ -8,63 +8,77 @@ from flask import current_app as APP
 # Import module model
 import app.catalog.model
 
-# Define the blueprint: 
+# Define the blueprint:
 catalog = Blueprint('catalog', __name__)
 
-#fake data
-catalog_list= [{'id':1, 'name':'catalog1', 'text':"321432"},{'id':2, 'name':'catalog2', 'text':"321432"}]
-catalog1={'id':1, 'name':'catalog1', 'text':"321432"}
-items_array=[{'id':1,'name':'item1','catalog_name':'catalog1','text':'43214321421342314231'},{'id':2,'name':'item2','catalog_name':'catalog1','text':'43214321421342314231'}]
-item1={'id':1,'name':'item1','catalog_name':'catalog1','text':'43214321421342314231'}
+# fake data
+catalog_list = [{'id': 1, 'name': 'catalog1', 'text': "321432"}, {
+    'id': 2, 'name': 'catalog2', 'text': "321432"}]
+catalog1 = {'id': 1, 'name': 'catalog1', 'text': "321432"}
+items_array = [{'id': 1, 'name': 'item1', 'catalog_name': 'catalog1', 'text': '43214321421342314231'}, {
+    'id': 2, 'name': 'item2', 'catalog_name': 'catalog1', 'text': '43214321421342314231'}]
+item1 = {'id': 1, 'name': 'item1', 'catalog_name': 'catalog1',"catalog_id":1,
+         'text': '43214321421342314231'}
 
-items_array2=[{'id':1,'name':'1111','catalog_name':'catalog1','text':'43214321421342314231'},{'id':2,'name':'2222','catalog_name':'catalog1','text':'43214321421342314231'}]
+items_array2 = [{'id': 1, 'name': '1111', 'catalog_name': 'catalog1', 'text': '43214321421342314231'}, {
+    'id': 2, 'name': '2222', 'catalog_name': 'catalog1', 'text': '43214321421342314231'}]
 
+# Make option list for catalog
+def get_option():
+    items=[]
+    for item in catalog_list:
+        items.append({'value':item['id'],'name':item['name']})
+    return items
 
 # Set the route and accepted methods
 @catalog.route('/')
 @catalog.route('/catalog/')
 def index():
-    return render_template('catalog/main_list.html',catalog=catalog_list,items=items_array)
+    return render_template('catalog/main_list.html', catalog=catalog_list, items=items_array)
     # try:
     #     return render_template('catalog/list.html',catalog=catalog_list)
     # except Exception, e:
     #     abort(404)
 
+
 @catalog.route('/catalog/<catalog>/items')
 def detail(catalog):
-    return render_template('catalog/list.html',catalog=catalog_list,catalog_name='abc catalog',items=items_array2)
+    return render_template('catalog/list.html', catalog=catalog_list, catalog_name='abc catalog', items=items_array2)
+
 
 @catalog.route('/catalog/<catalog>/<item>')
-def item_detail(catalog,item):
-    return render_template('catalog/item.html',item=item1)
+def item_detail(catalog, item):
+    return render_template('catalog/item.html', item=item1)
 
 
 @catalog.route('/catalog/create')
 def create():
-    return "Catalog create!"
+    return render_template('catalog/catalog_create.html')
+
 
 @catalog.route('/catalog/<catalog>/edit')
 def edit(catalog):
-        return "Catalog edit %s!" % (catalog,)
+    return render_template('catalog/catalog_edit.html', item=catalog1)
+
 
 @catalog.route('/catalog/<catalog>/delete')
 def delete(catalog):
-    return "Catalog delete %s !" %(catalog,)
+    return render_template('catalog/catalog_delete.html', catalog=catalog1)
 
 
-
-#catalog items handling
-
+# catalog items handling
 
 
-@catalog.route('/catalog/<catalog>/create')
+@catalog.route('/catalog/item_create')
 def item_create():
-    return "Catalog create!"
+    return render_template('catalog/item_create.html',option=get_option())
+
 
 @catalog.route('/catalog/<catalog>/<item>/edit')
-def item_edit(catalog,item):
-    return "Catalog(%s) edit %s!" % (catalog,item,)
+def item_edit(catalog, item):
+    return render_template('catalog/item_edit.html',option=get_option(),catalog=catalog,item=item1)
+
 
 @catalog.route('/catalog/<catalog>/<item>/delete')
-def item_delete(catalog,item):
-    return "Catalog(%s) delete %s !" %(catalog,item,)
+def item_delete(catalog, item):
+    return render_template('catalog/item_delete.html',catalog=catalog,item=item1)
