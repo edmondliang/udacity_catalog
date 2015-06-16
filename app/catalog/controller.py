4f211198f1e5724df3ebfcd08288a8a3c1118692
edmondliang <edmond.liang.sf@gmail.com>
2015-06-16 06:44:59 +0000

    feat: basic template

--- app/catalog/controller.py
+++ app/catalog/controller.py
@@ -1,24 +1,43 @@
 # Import flask dependencies
 from flask import Blueprint, request, render_template, \
-                  flash, g, session, redirect, url_for
+                  flash, g, session, redirect, url_for,abort
 
 # Import the database object from the main app module
 from app import db
+from flask import current_app as APP
+# Import module model
+import app.catalog.model
 
-# Define the blueprint: 'auth', set its url prefix: app.url/auth
+# Define the blueprint: 
 catalog = Blueprint('catalog', __name__)
+
+#fake data
+catalog_list= [{'id':1, 'name':'catalog1', 'text':"321432"},{'id':2, 'name':'catalog2', 'text':"321432"}]
+catalog1={'id':1, 'name':'catalog1', 'text':"321432"}
+items_array=[{'id':1,'name':'item1','catalog_name':'catalog1','text':'43214321421342314231'},{'id':2,'name':'item2','catalog_name':'catalog1','text':'43214321421342314231'}]
+item1={'id':1,'name':'item1','catalog_name':'catalog1','text':'43214321421342314231'}
+
+items_array2=[{'id':1,'name':'1111','catalog_name':'catalog1','text':'43214321421342314231'},{'id':2,'name':'2222','catalog_name':'catalog1','text':'43214321421342314231'}]
+
+
 # Set the route and accepted methods
 @catalog.route('/')
-def index():
-    return "Home page!"
-
 @catalog.route('/catalog/')
-def show():
-    return "Catalog list page!"
+def index():
+    return render_template('catalog/main_list.html',catalog=catalog_list,items=items_array)
+    # try:
+    #     return render_template('catalog/list.html',catalog=catalog_list)
+    # except Exception, e:
+    #     abort(404)
 
-@catalog.route('/catalog/<catalog>')
+@catalog.route('/catalog/<catalog>/items')
 def detail(catalog):
-    return "Catalog %s show " % (catalog,)
+    return render_template('catalog/list.html',catalog=catalog_list,catalog_name='abc catalog',items=items_array2)
+
+@catalog.route('/catalog/<catalog>/<item>')
+def item_detail(catalog,item):
+    return render_template('catalog/item.html',item=item1)
+
 
 @catalog.route('/catalog/create')
 def create():
@@ -35,13 +54,8 @@ def delete(catalog):
 
 
 #catalog items handling
-@catalog.route('/catalog/<catalog>/items')
-def items(catalog):
-    return "Catalog %s items show " % (catalog,)
 
-@catalog.route('/catalog/<catalog>/<item>')
-def item_detail(catalog,item):
-    return "Catalog (%s) items <%s> show " % (catalog,item,)
+
 
 @catalog.route('/catalog/<catalog>/create')
 def item_create():
